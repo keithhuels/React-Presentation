c96821e20cb242d38e2335a39915c76e66f43629
HootDunk <58009556+HootDunk@users.noreply.github.com>
2020-05-01 16:57:07 -0500

    edited the text on the sources page

--- src/components/Sources.js
+++ src/components/Sources.js
@@ -5,7 +5,7 @@ class Sources extends Component {
         return(
             <div>
                 <h1>Sources</h1>
-                <h2> <a href='https://www.udemy.com/course/react-js-and-redux-mastering-web-apps/'>Udemy</a></h2>
+                <h2> <a href='https://www.udemy.com/course/react-js-and-redux-mastering-web-apps/'>Udemy - "React JS Web Development - The Essentials Bootcamp"</a></h2>
                    <h2> <a href='https://www.w3schools.com/react/react_css.asp'>w3 Schools</a></h2>
                    <h2> <a href='https://stackoverflow.com/questions/18665171/how-to-make-the-main-content-div-fill-height-of-screen-with-css'>Stack Overflow</a></h2>
                     <h2> <a href='https://www.sitepoint.com/community/t/help-with-bullet-list-alignment-needed-please/44332'>sitepoint</a></h2>
