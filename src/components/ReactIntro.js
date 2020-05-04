fd4c2e88e899d24994a305f2a9f6cd113d76a6e3
HootDunk <58009556+HootDunk@users.noreply.github.com>
2020-05-04 14:50:47 -0500

    made improvements to react intro page

--- src/components/ReactIntro.js
+++ src/components/ReactIntro.js
@@ -2,26 +2,37 @@ import React, { Component } from 'react';
 import handshake from '../assets/handshake.png';
 import pillar from '../assets/pillar.jpg';
 import ScrollToTop from './ScrollToTop';
+import reactLogo from '../assets/react-black.png';
+import react_and_js from '../assets/js-react-orbit-color1.png';
 
 class ReactIntro extends Component {
 render() {
     return(
         <div>
-            <h1> "React is a Javascript library for building user interfaces" -reactjs.org</h1>
+        <hr />
+         <h1>React Introduction</h1>
+         <hr />
+         {/* <img src={handshake} alt='handshake' className='handshake'/>  */}
+         <li>Released in 2013, React is a JavaScript library that was designed by Facebook</li>
+        
+         
+         <img src={reactLogo} alt='logo' className='reactLogo'/>
 
-
-         <h2>React Introduction</h2> 
-         <img src={handshake} alt='handshake' className='handshake'/>
-    
+         
          <ul>
+            <h2>Quick Refresher</h2>
              <li>
                   There are three core pillars of web development: HTML, CSS, and Javascript
                   <p></p>
                   <img src={pillar} alt='pillar' className='pillar' />
              </li>
-             <p> </p>
+            <h2>How React fits into the picture</h2>
              <li>
-                 React is used in the Javascript side of web development.
+                 React is used on the javascript side of web development, as it is a JavaScript library.
+             </li>
+             <img src={react_and_js} alt='react and js image' className='react_and_js'/>
+             <li> 
+             It allows developers to compose complex user interfaces from small isolated pieces of code called "components"
              </li>
          </ul>
          
